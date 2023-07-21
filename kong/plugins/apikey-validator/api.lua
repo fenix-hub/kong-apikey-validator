457fdd659595ce465202bc9ffff7a4c489035696
Santilio Nicolo <santilio@aurigaactive.aurigainformatica.it>
2023-07-21 14:53:35 +0200

    update version to 0.2.11

--- kong/plugins/apikey-validator/api.lua
+++ kong/plugins/apikey-validator/api.lua
@@ -1,6 +1,15 @@
+-- workaround
+local EmptySchema = {}
+function EmptySchema:new()
+  local self = {}
+  function self.each_field(...) return function() end end
+  return self
+end
+
 return {
   ["/apikey-validator"] = {
     methods = {
+      schema = EmptySchema:new(),
       GET = function(self)
         local request = self.req
         kong.log("request: " .. request)
