4addb367f7a3a4e8c766823f133b33068379b928
Santilio Nicolo <santilio@aurigaactive.aurigainformatica.it>
2023-07-26 15:39:19 +0200

    update version to 0.2.13

--- kong/plugins/apikey-validator/api.lua
+++ kong/plugins/apikey-validator/api.lua
@@ -1,6 +1,34 @@
 local json = require "lunajson"
 
-local consumer_schema = kong.db.consumers.schema;
+local consumer_schema = {
+  name          = "consumers",
+  primary_key   = { "id" },
+  endpoint_key  = "username",
+  workspaceable = true,
+
+  fields        = {
+    {
+      username = {
+        description =
+        "The unique username of the Consumer. You must send either this field or custom_id with the request.",
+        type = "string",
+        unique = true
+      },
+    },
+    {
+      custom_id =
+      {
+        description = "Stores the existing unique ID of the consumer.",
+        type = "string",
+        unique = true
+      },
+    },
+  },
+
+  entity_checks = {
+    { at_least_one_of = { "custom_id", "username" } },
+  },
+}
 
 
 return {
@@ -8,8 +36,6 @@ return {
     schema = consumer_schema,
     methods = {
       GET = function(self)
-        kong.log(consumer_schema.fields.custom_id)
-        kong.log(json.encode(consumer_schema.fields))
         kong.log(consumer_schema.name)
         return kong.response.exit(200, { body = json.encode(consumer_schema.fields) })
       end,
