8ee9a169bf20348dca2d3e0d12a5467ffa737bce
Santilio Nicolo <santilio@aurigaactive.aurigainformatica.it>
2023-07-26 15:41:48 +0200

    update version to 0.2.13

--- kong/plugins/apikey-validator/api.lua
+++ kong/plugins/apikey-validator/api.lua
@@ -4,30 +4,7 @@ local consumer_schema = {
   name          = "consumers",
   primary_key   = { "id" },
   endpoint_key  = "username",
-  workspaceable = true,
-
-  fields        = {
-    {
-      username = {
-        description =
-        "The unique username of the Consumer. You must send either this field or custom_id with the request.",
-        type = "string",
-        unique = true
-      },
-    },
-    {
-      custom_id =
-      {
-        description = "Stores the existing unique ID of the consumer.",
-        type = "string",
-        unique = true
-      },
-    },
-  },
-
-  entity_checks = {
-    { at_least_one_of = { "custom_id", "username" } },
-  },
+  workspaceable = true
 }
 
 
@@ -37,7 +14,7 @@ return {
     methods = {
       GET = function(self)
         kong.log(consumer_schema.name)
-        return kong.response.exit(200, { body = json.encode(consumer_schema.fields) })
+        return kong.response.exit(200, { body = json.encode(consumer_schema.name) })
       end,
     },
   },
