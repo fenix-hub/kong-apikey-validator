3b7f8338761bc20f74207a821523dbb483199a4c
Santilio Nicolo <santilio@aurigaactive.aurigainformatica.it>
2023-07-26 16:17:07 +0200

    update version to 0.2.13

--- kong/plugins/apikey-validator/api.lua
+++ kong/plugins/apikey-validator/api.lua
@@ -1,8 +1,9 @@
 return {
   ["/apikey/generate"] = {
+    schema = {},
     methods = {
       GET = function()
-        return kong.response.exit(200)
+        return kong.response.exit(200, { message = "Hello World!" })
       end,
     },
   },
