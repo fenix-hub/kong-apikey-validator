32c2a457043b7e3ee0d8da88e39be5561f9d3d82
Santilio Nicolo <santilio@aurigaactive.aurigainformatica.it>
2023-07-26 15:52:38 +0200

    update version to 0.2.13

--- kong/plugins/apikey-validator/api.lua
+++ kong/plugins/apikey-validator/api.lua
@@ -5,15 +5,13 @@ function bar()
   }
 end
 
-local consumer_schema = bar()
+consumer_schema = bar()
 
 return {
   ["/apikey/generate"] = {
     schema = consumer_schema,
     methods = {
       GET = function(self)
-        kong.log(consumer_schema.fields)
-        kong.log(consumer_schema.name)
         return kong.response.exit(200)
       end,
     },
