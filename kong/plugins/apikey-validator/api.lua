3501a9cf7e10c050da087b3c16701ab92ead67cd
Santilio Nicolo <santilio@aurigaactive.aurigainformatica.it>
2023-07-26 15:43:11 +0200

    update version to 0.2.13

--- kong/plugins/apikey-validator/api.lua
+++ kong/plugins/apikey-validator/api.lua
@@ -1,20 +1,13 @@
-local json = require "lunajson"
-
-local consumer_schema = {
-  name          = "consumers",
-  primary_key   = { "id" },
-  endpoint_key  = "username",
-  workspaceable = true
-}
-
+local consumer_schema = kong.db.consumers.schema;
 
 return {
   ["/apikey/generate"] = {
     schema = consumer_schema,
     methods = {
       GET = function(self)
+        kong.log(consumer_schema.fields)
         kong.log(consumer_schema.name)
-        return kong.response.exit(200, { body = json.encode(consumer_schema.name) })
+        return kong.response.exit(200)
       end,
     },
   },
