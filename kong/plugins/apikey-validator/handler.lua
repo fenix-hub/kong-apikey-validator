0666db9e84cf35b00bd519d77cecd3404da9984c
Santilio Nicolo <santilio@aurigaactive.aurigainformatica.it>
2023-07-27 17:25:52 +0200

    update version to 0.3.2

--- kong/plugins/apikey-validator/handler.lua
+++ kong/plugins/apikey-validator/handler.lua
@@ -21,7 +21,7 @@ local rate_limiting_logics = require "kong.plugins.apikey-validator.rate-limitin
 
 local ApikeyValidator = {
   PRIORITY = 1000, -- set the ApikeyValidator priority, which determines ApikeyValidator execution order
-  VERSION = "0.3.1", -- version in X.Y.Z format. Check hybrid-mode compatibility requirements.
+  VERSION = "0.3.2", -- version in X.Y.Z format. Check hybrid-mode compatibility requirements.
 }
 
 -- do initialization here, any module level code runs in the 'init_by_lua_block',
@@ -97,6 +97,14 @@ function ApikeyValidator:access(conf)
     body = json.encode(body),
     headers = headers,
   })
+
+  if err then
+    kong.log.err("Error: " .. err)
+    return kong.response.error(500, { message = "Internal server error" }, headers)
+  end
+
+  local prefix, _ = apikey:match("([^.]*)%.(.*)")
+  apikey = nil
 
   kong.log.debug("Response: " .. response.body .. " " .. response.status)
 
