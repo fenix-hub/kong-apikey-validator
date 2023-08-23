ad78c0e468320635a991d61342c6cecf5bab278a
Santilio Nicolo <santilio@aurigaactive.aurigainformatica.it>
2023-08-23 18:30:20 +0200

    update version to 0.5.1

--- kong/plugins/apikey-validator/handler.lua
+++ kong/plugins/apikey-validator/handler.lua
@@ -169,7 +169,7 @@ function ApikeyValidator:access(conf)
     ["X-Saatisfied-Payment-Configuration"] = apikey_info["contract"],
   }
 
-  kong.request.set_headers(saatisfied_unauthorized)
+  kong.service.request.set_headers(saatisfied_unauthorized)
 
 
   ---------- [rate limiting phase] ------------
